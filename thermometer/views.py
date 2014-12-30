0ac0de6321cfe8e2e8c5001aab721e65d43aba8c
Bartosz Boguniewicz <b.boguniewicz@gmail.com>
2014-12-30 20:17:19 +0100

    Small cleanup

--- thermometer/views.py
+++ thermometer/views.py
@@ -15,10 +15,8 @@ class MainView(TemplateView):
     def get_context_data(self, **kwargs):
 
         context = super(MainView, self).get_context_data(**kwargs)
-        print context
         context['thermometers'] = Thermometer.objects.all()
         context['sensors'] = Sensor.objects.all()
-        print context
         return context
 
     def get(self, request, *args, **kwargs):
