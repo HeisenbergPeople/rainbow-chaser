91e546d525d72b8d962fb5bef835a45b0d81289c
Karol Marcjan <karol.marcjan@gmail.com>
2015-04-16 10:53:29 +0200

    Add SensorType model in sensor.core

--- sensor/core/models.py
+++ sensor/core/models.py
@@ -5,18 +5,24 @@
 from django.db import models
 
 
+class SensorType(models.Model):
+    """Represents a type of sensors."""
+
+    name = models.CharField(max_length=128, unique=True)
+
+
 class GenericSensor(models.Model):
     """Represents a sensor abstracting away the specifics of what it measures.
 
     A sensor measures one kind of thing. A physical device might have
-    multiple logical sensors.
+    multiple logical sensors belonging to different sensor types.
     """
 
     name = models.CharField(max_length=256)
-    model = models.CharField(max_length=128)
+    sensor_type = models.ForeignKey('core.SensorType')
 
     class Meta:
-        unique_together = [('name', 'model')]
+        unique_together = [('name', 'sensor_type')]
 
 
 class Sensor(models.Model):
