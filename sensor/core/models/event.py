3385fe45b9d79710d5806bece6cf8b392ac8ec6e
Karol Marcjan <karol.marcjan@gmail.com>
2015-04-16 11:27:22 +0200

    Add Event.value_to_string method

--- sensor/core/models/event.py
+++ sensor/core/models/event.py
@@ -5,6 +5,9 @@
 from django.db import models
 
 
+VALUE_MAX_LEN = 128
+
+
 class GenericEvent(models.Model):
     """Represents a measurement event abstracting away what exactly is
     measured.
@@ -12,13 +15,21 @@ class GenericEvent(models.Model):
 
     sensor = models.ForeignKey('core.GenericSensor')
     datetime = models.DateTimeField()
-    value = models.CharField(max_length=128)
+    value = models.CharField(max_length=VALUE_MAX_LEN)
 
 
 class Event(models.Model):
     """Base class for sensor-specific event types"""
 
     generic_event = models.OneToOneField('core.GenericEvent')
+
+    def value_to_string(self):
+        """Event.value_to_string() -> unicode
+
+        Returns a string representation of the
+        """
+
+        raise NotImplementedError(self.__class__.value_to_string)
 
     class Meta:
         abstract = True
