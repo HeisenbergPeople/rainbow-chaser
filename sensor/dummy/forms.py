00f631419db248ad9ce0e4239e7b2c2aeec6a6cf
Karol Marcjan <karol.marcjan@gmail.com>
2015-04-30 13:08:44 +0200

    Add DummyEventUploadForm class

---
+++ sensor/dummy/forms.py
@@ -0,0 +1,15 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# -*- coding: utf-8 -*-
+
+from django.forms import ModelForm
+from sensor.dummy.models import DummyEvent
+
+
+class DummyEventUploadForm(ModelForm):
+
+    class Meta:
+        model = DummyEvent
+        fields = ('sensor', 'datetime', 'value')
