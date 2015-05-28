e4a1c33e18a49dbbb2e3850607a9fac5dd453a90
Bartosz Boguniewicz <b.boguniewicz@gmail.com>
2015-05-28 15:22:36 +0200

    Add create_if_not_exist_sensor_type

--- sensor/dummy/tests/test_dummy_sensor.py
+++ sensor/dummy/tests/test_dummy_sensor.py
@@ -3,6 +3,16 @@ from sensor.dummy.models import DummySensor
 
 __author__ = 'bradley'
 
+
+def create_if_not_exist_sensor_type(name):
+    sensor_types = SensorType.objects.filter(name=name)
+    if len(sensor_types) == 0:
+        sensor_type = SensorType(name=name)
+        sensor_type.save()
+        return sensor_type
+    return sensor_types[0]
+
+
 class TestDummySensor(TestCase):
 
     def test_dummy_sensor_create(self):
